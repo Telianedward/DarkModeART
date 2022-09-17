0fde59e5678dfb3da467c568dfe06942e62e95a1
edward <Telianedward@icloud.com>
2022-09-18 01:11:04 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/g.js
+++ src/g.js
@@ -1,39 +1,57 @@
 
 
 
-`${themes({ light: `#${backgroundColor}${opacity[]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
-`${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
-`${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
-`${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
-`${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
-`${themes({ light: `#${backgroundColor}${opacity[4]}`, dark: `#${foregroundColor}${opacity[4]}`, dimmed: `#${backgroundColor}${opacity[5]}`, hc: `#${foregroundColor}${opacity[5]}`, system: `#${systemForeground}${opacity[4]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[4]}`, dark: `#${foregroundColor}${opacity[4]}`, dimmed: `#${backgroundColor}${opacity[5]}`, hc: `#${foregroundColor}${opacity[5]}`, system: `#${systemForeground}${opacity[4]}` })}}`,
 
-`${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
-
-
-`${themes({ light: `#${backgroundColor}${opacity[6]}`, dark: `#${foregroundColor}${opacity[6]}`, dimmed: `#${backgroundColor}${opacity[7]}`, hc: `#${foregroundColor}${opacity[7]}`, system: `#${systemForeground}${opacity[6]}` })}}`,
-`${themes({ light: `#${backgroundColor}${opacity[7]}`, dark: `#${foregroundColor}${opacity[7]}`, dimmed: `#${backgroundColor}${opacity[8]}`, hc: `#${foregroundColor}${opacity[8]}`, system: `#${systemForeground}${opacity[7]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[5]}`, dark: `#${foregroundColor}${opacity[5]}`, dimmed: `#${backgroundColor}${opacity[6]}`, hc: `#${foregroundColor}${opacity[6]}`, system: `#${systemForeground}${opacity[5]}` })}}`,
 
 
-danger.muted
-`#${hex(color.danger.muted)}${opacity[3]}`,
-`#${hex(color.danger.muted)}${opacity[4]}`,
-`#${hex(color.danger.muted)}${opacity[5]}`,
-`#${hex(color.danger.muted)}${opacity[6]}`,
-`#${hex(color.danger.muted)}${opacity[7]}`,
-`#${hex(color.danger.muted)}${opacity[8]}`,
-`#${hex(color.danger.muted)}${opacity[9]}`,
-`#${hex(color.danger.muted)}${opacity[9]}`,
-scale.yellow.0
-`#${hex(color.scale.yellow[0])}${opacity[8]}`,
+// `${themes({ light: `#${backgroundColor}${opacity[6]}`, dark: `#${foregroundColor}${opacity[6]}`, dimmed: `#${backgroundColor}${opacity[7]}`, hc: `#${foregroundColor}${opacity[7]}`, system: `#${systemForeground}${opacity[6]}` })}}`,
+// `${themes({ light: `#${backgroundColor}${opacity[7]}`, dark: `#${foregroundColor}${opacity[7]}`, dimmed: `#${backgroundColor}${opacity[8]}`, hc: `#${foregroundColor}${opacity[8]}`, system: `#${systemForeground}${opacity[7]}` })}}`,
 
-`#${hex(color.success.muted)}${opacity[9]}`,
-`#${hex(color.success.muted)}${opacity[7]}`,
 
-`#${hex(color.accent.muted)}${opacity[7]}`,
-`#${hex(color.done.emphasis)}}${opacity[5]}`,
+// danger.muted
+// `#${hex(color.danger.muted)}${opacity[3]}`,
+// `#${hex(color.danger.muted)}${opacity[4]}`,
+// `#${hex(color.danger.muted)}${opacity[5]}`,
+// `#${hex(color.danger.muted)}${opacity[6]}`,
+// `#${hex(color.danger.muted)}${opacity[7]}`,
+// `#${hex(color.danger.muted)}${opacity[8]}`,
+// `#${hex(color.danger.muted)}${opacity[9]}`,
+// `#${hex(color.danger.muted)}${opacity[9]}`,
+// scale.yellow.0
+// `#${hex(color.scale.yellow[0])}${opacity[8]}`,
 
-done.emphasis
+// `#${hex(color.success.muted)}${opacity[9]}`,
+// `#${hex(color.success.muted)}${opacity[7]}`,
 
-`#${hex(color.sponsors.muted)}}`,
-sponsors.muted
+// `#${hex(color.accent.muted)}${opacity[7]}`,
+// `#${hex(color.done.emphasis)}}${opacity[5]}`,
+
+// done.emphasis
+
+// `#${hex(color.sponsors.muted)}}${opacity[0]}`,
+// sponsors.muted
+// `#${hex(color.scale.blue[0])}}${opacity[0]}`,
+
+
+// `#${hex(color.success.muted)}${opacity[8]}`,
+
+// `#${hex(color.danger.muted)}${opacity[0]}`,
+
+// `#${hex(color.severe.muted)}${opacity[5]}`,  - оранж
+
+// `#${hex(color.done.muted )}${opacity[10]}`,
+
+// `#${hex(color.scale.yellow[0])}}${opacity[10]}`,
+// severe.muted
+
+// done.muted - фиолет
+
+
+// themes({ light: `#${foregroundUnderColor}${opacity[7]}`, dark: `#${backgroundOnColor}${opacity[7]}`, dimmed: `#${foregroundUnderColor}${opacity[6]}`, hc: `#${backgroundOnColor}${opacity[6]}`, system: `#${systemONBackground}${opacity[7]}` }),
