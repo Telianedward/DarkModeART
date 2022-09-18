472c1f1621a1f09c31ba16da9ebd98de03a2160d
edward <Telianedward@icloud.com>
2022-09-19 01:09:19 +0300

    fx
    Signed-off-by: edward <Telianedward@icloud.com>

--- src/colors.js
+++ src/colors.js
@@ -1,13 +1,40 @@
-const l = require("@primer/primitives/dist/json/colors/light-default.json"),
-      lHC = require("@primer/primitives/dist/json/colors/light_high_contrast.json"),
-      lC = require("@primer/primitives/dist/json/colors/light_colorblind.json"),
-      lT = require("@primer/primitives/dist/json/colors/light_tritanopia.json"),
-      d = require("@primer/primitives/dist/json/colors/dark.json"),
-      dD = require("@primer/primitives/dist/json/colors/dark_dimmed.json"),
-      dHC  = require("@primer/primitives/dist/json/colors/dark_high_contrast.json"),
-      dC = require("@primer/primitives/dist/json/colors/dark_colorblind.json"),
-      dT = require("@primer/primitives/dist/json/colors/dark_tritanopia.json"),
-      S = require("@primer/primitives/dist/json/colors/dark.json")
-      _gC = (t) => {return t === "l"?l:(t === "lhc"?lHC:(t === "lc"?lC:(t === "lt"?lT:(t === "d"?d:(t === "dd"?dD:(t === "dhc"?dHC:(t === "dc"?dC:(t === "dt"?dT:(t === "s"?S:false)))))))))}
-      module.exports = {_gC};
+const _l = require("@primer/primitives/dist/json/colors/light.json");
+const  _lHC = require("@primer/primitives/dist/json/colors/light_high_contrast.json");
+const  _lC = require("@primer/primitives/dist/json/colors/light_colorblind.json");
+const   _lT = require("@primer/primitives/dist/json/colors/light_tritanopia.json");
+const _d = require("@primer/primitives/dist/json/colors/dark.json");
+const   _dD = require("@primer/primitives/dist/json/colors/dark_dimmed.json");
+const _dHC  = require("@primer/primitives/dist/json/colors/dark_high_contrast.json");
+const  _dC = require("@primer/primitives/dist/json/colors/dark_colorblind.json");
+const _dT = require("@primer/primitives/dist/json/colors/dark_tritanopia.json");
+const  _S = require("@primer/primitives/dist/json/colors/dark.json");
+    //   _gC = (t) => {return t === "l"?l:(t === "lhc"?lHC:(t === "lc"?lC:(t === "lt"?lT:(t === "d"?d:(t === "dd"?dD:(t === "dhc"?dHC:(t === "dc"?dC:(t === "dt"?dT:(t === "s"?S:false)))))))))}
+    //   module.exports = {_gC};
 
+    function getColors(theme) {
+        if (theme === "l") {
+          return _l;
+        } else if (theme === "lhc") {
+          return _lHC;
+        } else if (theme === "lc") {
+          return _lC;
+        } else if (theme === "lt") {
+          return _lT;
+        }else if (theme === "d") {
+            return _d;
+          } else if (theme === "dd") {
+            return _dD;
+          } else if (theme === "dhc") {
+            return _dHC;
+          } else if (theme === "dc") {
+            return _dC;
+          }else if (theme === "dt") {
+            return _dT;
+          } else if (theme === "s") {
+            return _S;
+          }
+      }
+      
+      module.exports = {
+        getColors,
+      };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const fs = require("fs").promises,
-      _gT = require("./theme"),
-      l = _gT({ theme: "l", name: "AtomAx mode Light"}),
-      lHC = _gT({ theme: "lhc",name: "AtomAx mode Light High Contrast"}),
-      lC = _gT({theme: "lc", name: "AtomAx mode Light Colorblind"}),
-      lT = _gT({theme: "lt", name: "AtomAx mode Light Tritanopia"}),
-      d = _gT({ theme: "d", name: "AtomAx mode Dark"}),
-      dD = _gT({ theme: "dd", name: "AtomAx mode Dark Dimmed"}),
-      dHC = _gT({theme: "dhc", name: "AtomAx mode Dark High Contrast"}),
-      dC = _gT({theme: "dc", name: "AtomAx mode Dark Colorblind"}),
-      dT = _gT({theme: "dt", name: "AtomAx mode Dark Tritanopia"}),
-      S = _gT({theme: "s", name: "AtomAx mode System Themes"})
+const fs = require("fs").promises;
+const   _gT = require("./theme");
+const   l = _gT({ theme: "l", name: "AtomAx mode Light"});
+const    lHC = _gT({ theme: "lhc",name: "AtomAx mode Light High Contrast"});
+const    lC = _gT({theme: "lc", name: "AtomAx mode Light Colorblind"});
+const  lT = _gT({theme: "lt", name: "AtomAx mode Light Tritanopia"});
+const  d = _gT({ theme: "d", name: "AtomAx mode Dark"});
+const   dD = _gT({ theme: "dd", name: "AtomAx mode Dark Dimmed"});
+const   dHC = _gT({theme: "dhc", name: "AtomAx mode Dark High Contrast"});
+const   dC = _gT({theme: "dc", name: "AtomAx mode Dark Colorblind"});
+const  dT = _gT({theme: "dt", name: "AtomAx mode Dark Tritanopia"});
+const   S = _gT({theme: "s", name: "AtomAx mode System Themes"});
 
 
 
 
       fs.mkdir("./themes", { recursive: true })
         .then(() => Promise.all([
-          fs.writeFile("./themes/light-default.json", JSON.stringify(l, null, 2)),
+          fs.writeFile("./themes/light.json", JSON.stringify(l, null, 2)),
           fs.writeFile("./themes/light_high_contrast.json", JSON.stringify(lHC, null, 2)),
           fs.writeFile("./themes/light_colorblind.json", JSON.stringify(lC , null, 2)),
           fs.writeFile("./themes/light_tritanopia.json", JSON.stringify(lT , null, 2)),
